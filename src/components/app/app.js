d337234f7584969c39c29814d8982c35074f374e
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-10-06 14:00:36 -0700

    mobile

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,6 +14,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="app">
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <BrowserRouter>
           <div>
             <Favicon url={ favicon } />
